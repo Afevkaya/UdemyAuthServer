d1eea04d2fbaff7a0203b373b20c4146e3bd5a12
Afevkaya <Afevkaya5151@gmail.com>
2022-04-03 16:06:27 +0300

    Shared Layer: ErrorDto nesnesinin oluşturulması.

---
+++ SharedLibrary/DTOs/ErrorDto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.DTOs
+{
+    // ErrorDto class
+    public class ErrorDto
+    {
+        public List<string> Errors { get; private set; }
+        public bool IsShow { get; set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error, bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            this.Errors = errors;
+            this.IsShow = isShow;
+        }
+    }
+}
