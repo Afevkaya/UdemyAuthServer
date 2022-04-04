36d9db3f566ab0a783de7d5e51b3f0ff62762146
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-04 13:19:52 +0300

    IUserService interface'inin oluşturulması

---
+++ UdemyAuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,16 @@
+﻿using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UdemyAuthServer.Core.DTOs;
+
+namespace UdemyAuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<CustomResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<CustomResponseDto<UserAppDto>> GetUserByNameAsync(string userName);
+    }
+}
