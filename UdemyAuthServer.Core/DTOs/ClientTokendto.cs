6ebd61e52a4299067eb9fd6947c0b5590e95088f
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-04 10:53:33 +0300

    TokenDto/ClientTokenDto classlarının oluşturulması

---
+++ UdemyAuthServer.Core/DTOs/ClientTokendto.cs
@@ -0,0 +1,9 @@
+﻿namespace UdemyAuthServer.Core.DTOs
+{
+    public class ClientTokenDto
+    {
+        public string RefreshToken { get; set; }
+        public DateTime ResfreshTokenExpiration { get; set; }
+
+    }
+}
