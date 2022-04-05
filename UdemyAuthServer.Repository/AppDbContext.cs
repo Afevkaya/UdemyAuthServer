bb8f1c89b56c73a49c59a61e4ad986a8a780c4fe
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-05 11:03:04 +0300

    Repository Layer: AppDbContext class'ını repo katmanına eklenmesi

---
+++ UdemyAuthServer.Repository/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using UdemyAuthServer.Core.Models;
+
+namespace UdemyAuthServer.Repository
+{
+    // Db ile entity classları maplediğimiz class.
+    // Bu sefer IdentityDbContext classı inherit ettik.
+    // User ile ilgili DbSetler hazır olduğu için geri kalan entityleri mapledik.
+
+    // AppDbContext class.
+    public class AppDbContext : IdentityDbContext<UserApp,IdentityRole,string>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
