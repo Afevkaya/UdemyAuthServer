c00657ec0c5025fcdd4072548b007d5010863b73
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-06 11:15:19 +0300

    Service Layer: Options Pattern uygulanması

---
+++ UdemyAuthServer.Service/Services/SignService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace UdemyAuthServer.Service.Services
+{
+    // Token içerisindeki secretkey'i şifreleyip geri döndüren class.
+
+    // Static SignService class
+    internal static class SignService
+    {
+        // Simetrik şifreleme metodu
+        public static SecurityKey GetSymmetricSecurityKey(string securityKey)
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
