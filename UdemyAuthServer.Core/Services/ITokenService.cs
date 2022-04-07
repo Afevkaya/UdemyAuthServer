3881c90cba93fc8dc0ced740f9d8ba806cfc5416
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-07 10:40:13 +0300

    Core Layer: ITokenService interface'inin güncellenmesi.

--- UdemyAuthServer.Core/Services/ITokenService.cs
+++ UdemyAuthServer.Core/Services/ITokenService.cs
@@ -4,9 +4,10 @@ using UdemyAuthServer.Core.Models;
 
 namespace UdemyAuthServer.Core.Services
 {
-    // Token üreten service interface
-    // Hem üyelik işlemi gerektiren apiler için token üretiyor hem de gerektirmeyen apiler için token üretiyor.
-    // Bu token service dış dünyaya açılmayacak. İçeride kullanılacak.
+    
+    // Token üretecek class'ın genel yapısını oluşturduğumuz interface
+    // Bu service hem üyelik işlemi gerektiren API'ler için hem de gerektirmeyen API'ler için token üretir.
+    // Kullanıcı ile direk iletişime geçmeyen bir service'dir.
 
     // ITokenService interface
     public interface ITokenService
