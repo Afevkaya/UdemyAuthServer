28a31dbc162bffd9b08839c48d7c4b8815d5561b
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-07 09:51:42 +0300

    Service Layer: UserService class'ının oluşturulması.

---
+++ UdemyAuthServer.Service/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using SharedLibrary.Dtos;
+using UdemyAuthServer.Core.DTOs;
+using UdemyAuthServer.Core.Models;
+using UdemyAuthServer.Core.Services;
+
+namespace UdemyAuthServer.Service.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly UserManager<UserApp> _userManager;
+
+        public UserService(UserManager<UserApp> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public Task<CustomResponseDto<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CustomResponseDto<UserAppDto>> GetUserByNameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
