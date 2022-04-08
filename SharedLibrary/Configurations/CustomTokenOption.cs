6597478f9240410d136cb459ee8097199204ef0d
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-08 13:57:56 +0300

    Service Layer: CustomTokenOption class'ının güncellenmesi

--- SharedLibrary/Configurations/CustomTokenOption.cs
+++ SharedLibrary/Configurations/CustomTokenOption.cs
@@ -1,5 +1,12 @@
 ﻿namespace SharedLibrary.Configurations
 {
+    // appsettings.json dosyası içerisinde Token'a ait key-value tipinde kritik bilgiler bulunur.
+    // Bu bilgilere proje içerisinde erişmek için oluşturulan class.
+    // Bu bilgileri bu class'a atayabilmek için option pattern uygulamamız gerekli.
+    // Option Pattern'ı da bütün projemizin ayağa kaltığı dosya olan program.cs dosyası içerisinde uyguluyoruz.
+
+
+    // CustomTokenOption class
     public class CustomTokenOption
     {
         public List<string> Audience { get; set; }
