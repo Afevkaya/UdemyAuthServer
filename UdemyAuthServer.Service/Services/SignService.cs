b035297d13d29826d48c83c8abc2f640b372a58f
Ahmet.evkaya <Ahmet.evkaya@ELD-PC-38>
2022-04-08 11:17:36 +0300

    Service Layer: SignService class'ının güncellenmesi.

--- UdemyAuthServer.Service/Services/SignService.cs
+++ UdemyAuthServer.Service/Services/SignService.cs
@@ -3,7 +3,7 @@ using System.Text;
 
 namespace UdemyAuthServer.Service.Services
 {
-    // Token içerisindeki secretkey'i şifreleyip geri döndüren class.
+    // Access Token ile geri dönecek key'i symmetric şekilde imzalayıp(şifreleme) şifreli bir şekilde döndürme.
 
     // Static SignService class
     internal static class SignService
